1e6eecd21c65e7b5040b8dde1b1acde17058f09c
Naveed Ziari <naveedziari@gmail.com>
2023-08-09 18:02:54 -0700

    Add files via upload

---
+++ mida_algorithm/correction.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+
+def normalization(iso_dist):
+	if iso_dist.ndim == 1:
+		output = iso_dist/np.sum(iso_dist)
+	else:
+		output = np.zeros(iso_dist.shape)
+		for row_index in range(iso_dist.shape[0]):
+			output[row_index,:] = [iso_dist[row_index,col_index]/np.sum(iso_dist[row_index,:]) for col_index in range(iso_dist.shape[1])]
+	return output
+
